cdb54704539b4a586a96ed5b928b57a7f206b606
ppesq <betoesquivel>
2014-02-24 14:17:39 -0600

    First Commit... Jala el Hello world...

---
+++ src/tiroparabolico/TiroParabolico.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tiroparabolico;
+
+/**
+ *
+ * @author ppesq
+ */
+public class TiroParabolico {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //José Alberto Esquivel A01139626   
+        //Hugo León Garza A01139720
+        JFrameTiroParabolico juegoTiro = new JFrameTiroParabolico();
+        juegoTiro.setVisible(true);
+    }
+    
+}
