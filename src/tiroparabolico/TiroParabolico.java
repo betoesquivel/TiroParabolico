d62c265e110f76d0d6d9f9d3c37024bf6e2e4fab
hlg <hlg@Adell>
2014-02-24 14:33:40 -0600

    Cambio indicaciones, repartí el trabajo

--- src/tiroparabolico/TiroParabolico.java
+++ src/tiroparabolico/TiroParabolico.java
@@ -8,16 +8,19 @@ package tiroparabolico;
 
 /**
  * Indicaciones: 
+ * -> lo de guardar lo hace Beto.
  * En jugando con java se ve en Score como guardar el juego en disco. Algo que va a
  * pasar cuando se presione g. Solamente se puede guardar un juego. El juego se va
  * a cargar cuando se presione la letra c. 
  * 
  * Tambien tenemos que agregar lo de pausa. 
  * 
- * Agregar fisica...
+ * Agregar fisica... -> Lo de física lo hace Hugo.
  * 
  * Recursos: Hay que conseguir una pelotita de sprite. Y también las plataformas.
- * o podemos pintar rectangulos...
+ * o podemos pintar rectangulos... 
+ * |-> podemos hacerlo con un jugador de basquetbol, un balon de basquetbol y una canasta
+ *      le haces click al jugador y sale el balon en tiro parabolico y debe atraparlo la canasta
  * @author ppesq
  */
 public class TiroParabolico {
